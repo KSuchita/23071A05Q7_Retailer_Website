6b01d2dd2155b99b20ea5ddd8724695e3c49dd69
KSuchita <kondetisuchita@gmail.com>
2025-05-20 11:06:24 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useState } from "react";
 import Register from "./Register";
 import Login from "./Login";
 import Catalog from "./Catalog";
 import Cart from "./Cart";
 import Payment from "./Payment";
 import Invoice from "./Invoice";
+import Contact from "./Contact";
 
 const App = () => {
-  const [cart, setCart] = useState([]);
-
-  const addToCart = (product) => {
-    setCart([...cart, product]);
-  };
-
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/catalog" element={<Catalog addToCart={addToCart} />} />
-        <Route path="/cart" element={<Cart cart={cart} />} />
+        <Route path="/catalog" element={<Catalog />} />
+        <Route path="/cart" element={<Cart />} />
         <Route path="/payment" element={<Payment />} />
         <Route path="/invoice" element={<Invoice order={{ id: 123, amount: 100 }} />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
   );
